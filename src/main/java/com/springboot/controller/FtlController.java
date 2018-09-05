aaf8ed7871ed7d938352a98392dcf02bdf276f45
zxd123 <826477396@qq.com>
2018-09-05 11:55:04 +0800

    2018-9-5 10:23 commit代码

---
+++ src/main/java/com/springboot/controller/FtlController.java
@@ -0,0 +1,16 @@
+package com.springboot.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class FtlController {
+
+    @RequestMapping("/ftl")
+public String ftltest(){
+
+    return "firstftl";
+}
+
+}
